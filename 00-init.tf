7d5352e33d9a6f809e5e4cb3a23749aa4072cbef
iromaniuk@advisor360.com <irek.romaniuk@gmail.com>
2021-01-24 11:24:20 -0500

    Initial

--- 00-init.tf
+++ 00-init.tf
@@ -1,3 +1,24 @@
 terraform {
-  required_version                                  = ">= 0.11.0"
+  required_providers {
+    panos = {
+      source = "paloaltonetworks/panos"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version   = ">= 2.0"
+    }
+  }
+  required_version = ">= 0.13"
+} 
+provider "azurerm" {
+  features {}
 }
+
+/*terraform {
+  backend "azurerm" {
+    resource_group_name  = "core-eastus-prod-tfstate"
+    storage_account_name = "sta360coretfstate0"
+    container_name       = "tfstate"
+    key                  = "panorama360/templates/tenant.tfstate"
+  }
+}*/
